c4062460f170ff56f882ae593af6bdc90aa49383
MonreeStudio <32088844+MonreeStudio@users.noreply.github.com>
2019-06-19 11:54:09 +0800

    Update CourseDesign.cpp

--- 社交关系图/CourseDesign.cpp
+++ 社交关系图/CourseDesign.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <queue>
+#include <list>
 #include <cstdlib>
 #include "Person.h"
 #include "AdjMatrix.h"
@@ -7,11 +8,36 @@ using namespace std;
 
 int main()
 {
-	AdjMatrix adjM;
-	if (adjM.CreateMatrix())
+	AdjMatrix* adjM = new AdjMatrix();
+	queue<string> Q;
+	if (adjM->CreateMatrix())
 	{
-		adjM.showAdj();
-		adjM.shortestPath();
+		for (int i = 0; i < 2; i++)
+		{
+			adjM->showAdj();
+			adjM->shortestPath();
+			cout << endl << "请输入需要查找附近联系人的人的序号(所有序号从1开始)：" << endl;
+			int find;
+			cin >> find;
+			find--;
+			cout << endl;
+			for (int j = 0; j < adjM->GetPersonNum(); j++)
+			{
+				if (adjM->GetAdj(find, j) < 100)
+				{
+					if(adjM->GetPersonName(j)!= adjM->GetPersonName(find))
+						Q.push(adjM->GetPersonName(j));
+				}
+			}
+			if (Q.empty())
+				cout << "哦豁，这个人附近没有人。" << endl;
+			while (!Q.empty())
+			{
+				cout << Q.front() << ' ';
+				Q.pop();
+			}
+			adjM->updateXY();
+		}
 	}
 	system("pause");
 }
