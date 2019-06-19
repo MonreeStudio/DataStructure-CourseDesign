c9821a9839eacccb04f1c59de6f6037637578846
MonreeStudio <32088844+MonreeStudio@users.noreply.github.com>
2019-06-20 00:32:51 +0800

    Update Person.h

--- 社交关系图/Person.h
+++ 社交关系图/Person.h
@@ -1,37 +1,36 @@
 #include<string>
 #include<ctime>
 #include<iostream>
-#include<Windows.h>
 using namespace std;
 #pragma once
 class Person
 {
-	friend class AdjMatrix;
+	friend class AdjMatrix;	//邻接矩阵AdjMatrix类定义为其友元类
 private:
 	string Name;//人名
 	int X;		//人所在X轴坐标
 	int Y;		//人所在Y轴坐标
 public:
-	Person()
+	Person()	//构造函数初始化成员变量
 	{
 		Name = "";
 		X = 0;
 		Y = 0;
 	}
-	Person(string a) 
+	Person(string a) //重载构造函数为成员变量赋值
 	{
 		this->Name = a;
 		InitX_Aixs();
 		InitY_Aixs();
 	}
-	void InitX_Aixs();
-	void InitY_Aixs();
-	string getName();
-	int getX();
-	int getY();
+	void InitX_Aixs();	//初始化X轴坐标
+	void InitY_Aixs();	//初始化Y轴坐标
+	string getName();	//获取名字
+	int getX();	//获取X轴坐标
+	int getY();	//获取Y轴坐标
 };
 
-void Person::InitX_Aixs()
+void Person::InitX_Aixs()	//利用随机数为X轴坐标赋值
 {
 	int tempX;
 	srand((unsigned)time(NULL)+Y);
@@ -39,7 +38,7 @@ void Person::InitX_Aixs()
 	X = tempX;
 }
 
-void Person::InitY_Aixs()
+void Person::InitY_Aixs()	//利用随机数为Y轴坐标赋值
 {
 	int tempY;
 	srand((unsigned)time(NULL)+X);
@@ -47,17 +46,17 @@ void Person::InitY_Aixs()
 	Y = tempY;
 }
 
-string Person::getName()
+string Person::getName()	//获取名字
 {
 	return Name;
 }
 
-int Person::getX()
+int Person::getX()	//获取X轴坐标
 {
 	return X;
 }
 
-int Person::getY()
+int Person::getY()	//获取Y轴坐标
 {
 	return Y;
 }
